73a8e4881093e318531c5d31ca78e2b9d5393883
nodza <noel.hwande@gmail.com>
2017-10-17 07:23:37 -0500

    Added styles for modal. Moved component styles to separate folders.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,6 +7,8 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleClearOptionModal}
     contentLabel="Selected Option"
+    closeTimeoutMS={200}
+    className="modal"
   >
     <h3>Selected Option</h3>
     {props.selectedOption && <p>{props.selectedOption}</p>}
